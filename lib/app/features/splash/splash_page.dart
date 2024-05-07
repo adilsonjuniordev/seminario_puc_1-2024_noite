1a0c61748b90d3af14c754929ff82100ff740610
Adilson Junior <adilson.pereira@w2o.com.br>
2024-05-07 19:09:20 -0300

    First commit

---
+++ lib/app/features/splash/splash_page.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('assets/images/logo.png'),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text('ENTRAR'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
